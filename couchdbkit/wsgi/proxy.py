a251e917f3372e405e53f74d22759f622503037c
benoitc <bchesneau@gmail.com>
2010-06-26 19:04:27 +0200

    provide a real WSGI application to proxy CouchDB.

--- couchdbkit/wsgi/proxy.py
+++ couchdbkit/wsgi/proxy.py
@@ -3,23 +3,43 @@
 # This file is part of couchdbkit released under the MIT license. 
 # See the NOTICE for more information.
 
+import urlparse
 
-from restkit.ext.wsgi_proxy import HostProxy, get_config
+from restkit.contrib.wsgi_proxy import HostProxy, get_config, ALLOWED_METHODS
+from restkit.pool.simple import SimplePool
+from webob import Request
 
-class CouchdbProxy(HostProxy):
-    """A proxy to redirect all request to CouchDB database"""
-    def __init__(self, db_name='', uri='http://127.0.0.1:5984',
-            allowed_methods=['GET'], **kwargs):
-        uri = uri.rstrip('/')
-        if db_name:
-            uri += '/' + db_name.strip('/')
-        super(CouchdbProxy, self).__init__(uri, allowed_methods=allowed_methods,
-                                        **kwargs)
-                                        
-def make_couchdb_proxy(global_config, db_name='', uri='http://127.0.0.1:5984',
-            **local_config):
-    """CouchdbProxy entry_point"""
-    uri = uri.rstrip('/')
-    config = get_config(local_config)
-    print 'Running CouchdbProxy on %s/%s with %s' % (uri, db_name, config)
-    return CouchdbProxy(db_name=db_name, uri=uri, **config)
+class CouchdbProxy(object):
+    """\
+    WSGI application to proxy a couchdb server.
+    
+    Simple usage to proxy a CouchDB server on default url::
+    
+        from couchdbkit.wsgi import CouchdbProxy
+        application = CouchdbProxy()
+    """
+    
+    def __init__(self, uri="http://127.0.0.1:5984", pool=None,
+            allowed_method=ALLOWED_METHODS, **local_config):
+        if not pool:
+            pool = SimplePool(keepalive=10)
+        config = get_config(local_config)
+        self.proxy = HostProxy(uri, pool=pool, 
+                        allowed_methods=allowed_method, **config)
+        
+    def do_proxy(self, req, environ, start_response):
+        """\
+        return proxy response. Can be overrided to add authentification and 
+        such. It's better to override do_proxy method than the __call__
+        """
+        return req.get_response(proxy)
+
+    def __call__(self, environ, start_response):
+        req = Request(environ)
+        if 'RAW_URI' in req.environ:
+            # gunicorn so we can use real path non encoded
+            u = urlparse.urlparse(req.environ['RAW_URI'])
+            req.environ['PATH_INFO'] = u.path
+            
+        resp = self.do_proy(req, environ, start_response)
+        return resp(environ, start_response)
