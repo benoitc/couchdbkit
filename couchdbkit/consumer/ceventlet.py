1ffcb9ed2759554d3ad417ef2e6b82821ddb4745
benoitc <bchesneau@gmail.com>
2010-09-20 11:55:30 +0200

    improve consumer & gevent consumers.

--- couchdbkit/consumer/ceventlet.py
+++ couchdbkit/consumer/ceventlet.py
@@ -3,26 +3,106 @@
 # This file is part of couchdbkit released under the MIT license. 
 # See the NOTICE for more information.
 
-import select
+import traceback
 
-from couchdbkit.consumer.async import AsyncConsumer 
+
+from couchdbkit.consumer.base import check_callable
+from couchdbkit.consumer.sync import SyncConsumer
 from couchdbkit.utils import json
 
 import eventlet
-from eventlet.hubs import trampoline
+from eventlet.greenthread import GreenThread
+from eventlet import event
 
-class EventletConsumer(AsyncConsumer):
-    def __init__(self, db, spawn=None):
-        if spawn is None:
-            spawn = eventlet.spawn_n
-        eventlet.monkey_patch(socket=True, select=True)
-        super(EventletConsumer, self).__init__(db, spawn=spawn)
+class ChangeConsumer(object):
+    def __init__(self, db, callback, **params):
+        self.process_change = callback
+        self.params = params
+        self.db = db
+        self.stop_event = event.Event()
 
-    def sleep(self, t):
-        eventlet.sleep(t)
+    def wait(self):
+        _ = eventlet.spawn_n(self._run)
+        self.stop_event.wait()
 
-    def wait_read(self, sock):
-        res = select.select([sock.fileno()], [], [], 1.0)
-        if res[0]:
-            return True
-        return False
+    def _run(self):
+
+        while True:
+            try:
+                resp = self.db.res.get("_changes", **self.params)
+                return self.consume(resp)
+            except (SystemExit, KeyboardInterrupt):
+                eventlet.sleep(5)
+                break
+            except:
+                traceback.print_exc()
+                eventlet.sleep(5)
+                break
+        self.stop_event.send(True)
+
+    def consume(self, resp):
+        raise NotImplementedError
+
+class ContinuousChangeConsumer(ChangeConsumer):
+
+    def consume(self, resp):
+        with resp.body_stream() as body:
+            while True:
+                line = body.readline()
+                if not line:
+                    break
+                if line.endswith("\r\n"):
+                    line = line[:-2]
+                else:
+                    line = line[:-1]
+                self.process_change(line)
+            self.stop_event.send(True)
+
+
+class LongPollChangeConsumer(ChangeConsumer):
+
+    def consume(self, resp):
+        with resp.body_stream() as body:
+            buf = []
+            while True:
+                data = body.read()
+                if not data:
+                    break
+                buf.append(data)
+            change = "".join(buf)
+            try:
+                change = json.loads(change)
+            except ValueError:
+                pass 
+            self.process_change(change)
+            self.stop_event.send(True)
+
+
+class EventletConsumer(SyncConsumer):
+    def __init__(self, db):
+        eventlet.monkey_patch(socket=True)
+        super(EventletConsumer, self).__init__(db)
+
+    def fetch(self, cb=None, **params):
+        if cb is None:
+            return super(EventletConsumer, self).wait_once(**params)
+        resp = self.db.res.get("_changes", **params)
+        eventlet.spawn_n(cb, resp.json_body)
+        eventlet.sleep(0.1)
+        
+    def wait_once(self, cb=None, **params):
+        if cb is None:
+            return super(EventletConsumer, self).wait_once(**params)
+
+        check_callable(cb)
+        params.update({"feed": "longpoll"})
+        consumer = LongPollChangeConsumer(self.db, callback=cb,
+                **params)
+        consumer.wait()
+
+    def wait(self, cb, **params):
+        params.update({"feed": "continuous"})
+        consumer = ContinuousChangeConsumer(self.db, callback=cb, 
+                **params)
+        consumer.wait()
+
--- couchdbkit/consumer/cgevent.py
+++ couchdbkit/consumer/cgevent.py
@@ -3,31 +3,99 @@
 # This file is part of couchdbkit released under the MIT license. 
 # See the NOTICE for more information.
 
+import traceback
 
 
-from couchdbkit.consumer.async import AsyncConsumer 
+from couchdbkit.consumer.base import check_callable
+from couchdbkit.consumer.sync import SyncConsumer
 from couchdbkit.utils import json
 
 import gevent
+from gevent import monkey 
+
 from gevent import socket
 from gevent import select
 
-class GeventConsumer(AsyncConsumer):
-    def __init__(self, db, spawn=None):
-        if spawn is None:
-            spawn = gevent.spawn
 
-        super(GeventConsumer, self).__init__(db, spawn=spawn)
+class ChangeConsumer(gevent.Greenlet):
+    def __init__(self, db, callback=None, **params):
+        gevent.Greenlet.__init__(self)
+        self.process_change = callback
+        self.params = params
+        self.db = db
 
-    def sleep(self, t):
-        gevent.sleep(t)
+    def _run(self):
+        while True:
+            try:
+                resp = self.db.res.get("_changes", **self.params)
+                return self.consume(resp)
+            except (SystemExit, KeyboardInterrupt):
+                gevent.sleep(5)
+            except:
+                traceback.print_exc()
+                gevent.sleep(5)
 
-    def wait_read(self, sock):
-        res = select.select([sock.fileno()], [], [], 1.0)
-        if res[0]:
-            return True
-        return False
+    def consume(self, resp):
+        raise NotImplementedError
 
-    def patch_socket(self, sock):
-        sock = socket.socket(_sock=sock)
-        return sock
+class ContinuousChangeConsumer(ChangeConsumer):
+
+    def consume(self, resp):
+        with resp.body_stream() as body:
+            while True:
+                line = body.readline()
+                if not line:
+                    break
+                if line.endswith("\r\n"):
+                    line = line[:-2]
+                else:
+                    line = line[:-1]
+                self.process_change(line)
+
+class LongPollChangeConsumer(ChangeConsumer):
+
+    def consume(self, resp):
+        with resp.body_stream() as body:
+            buf = []
+            while True:
+                data = body.read()
+                if not data:
+                    break
+                buf.append(data)
+                gevent.sleep(0.1)
+            change = "".join(buf)
+            try:
+                change = json.loads(change)
+            except ValueError:
+                pass 
+            self.process_change(change)
+
+
+class GeventConsumer(SyncConsumer):
+    def __init__(self, db):
+        monkey.patch_socket()
+        super(GeventConsumer, self).__init__(db)
+
+    def fetch(self, cb=None, **params):
+        if cb is None:
+            return super(GeventConsumer, self).wait_once(**params)
+        resp = self.db.res.get("_changes", **params)
+        gevent.spawn(cb, resp.json_body).join()
+        
+    def wait_once(self, cb=None, **params):
+        if cb is None:
+            return super(GeventConsumer, self).wait_once(**params)
+
+        check_callable(cb)
+        params.update({"feed": "longpoll"})
+        ret = LongPollChangeConsumer.spawn(self.db, callback=cb,
+                **params).join()
+        if cb is None:
+            return ret
+
+    def wait(self, cb, **params):
+        params.update({"feed": "continuous"})
+        ContinuousChangeConsumer.spawn(self.db, callback=cb, 
+                **params).join()
+        
+
