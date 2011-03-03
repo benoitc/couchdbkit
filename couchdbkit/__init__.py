7e970c94c9a373a2e17ae746838c36b4c9e97aa8
benoitc <bchesneau@gmail.com>
2011-03-03 14:29:04 +0100

    bump

--- couchdbkit/__init__.py
+++ couchdbkit/__init__.py
@@ -3,7 +3,7 @@
 # This file is part of couchdbkit released under the MIT license. 
 # See the NOTICE for more information.
 
-version_info = (0, 5, 4)
+version_info = (0, 5, 5)
 __version__ =  ".".join(map(str, version_info))
 
 try:
