6efd683177f38dcad7410e521a64ffd12547bacd
benoitc <bchesneau@gmail.com>
2012-01-25 23:40:38 -0800

    bump to 0.6.0

--- couchdbkit/__init__.py
+++ couchdbkit/__init__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -
 #
-# This file is part of couchdbkit released under the MIT license. 
+# This file is part of couchdbkit released under the MIT license.
 # See the NOTICE for more information.
 
 from .version import version_info, __version__
 
 try:
     from .resource import  RequestFailed, CouchdbResource
-
     from .exceptions import InvalidAttachment, DuplicatePropertyError,\
 BadValueError, MultipleResultsFound, NoResultFound, ReservedWordError,\
 DocsPathNotFound, BulkSaveError, ResourceNotFound, ResourceConflict, \
 PreconditionFailed
 
     from .client import Server, Database, ViewResults, View, TempView
+    from .changes import ChangesStream
     from .consumer import Consumer
     from .designer import document, push, pushdocs, pushapps, clone
     from .external import External
     from .loaders import BaseDocsLoader, FileSystemDocsLoader
-    
+
     from .schema import Property, Property, IntegerProperty,\
 DecimalProperty, BooleanProperty, FloatProperty, DateTimeProperty,\
 DateProperty, TimeProperty, dict_to_json, dict_to_json, dict_to_json,\
@@ -32,7 +32,7 @@ except ImportError:
     import traceback
     traceback.print_exc()
 
-import logging    
+import logging
 
 LOG_LEVELS = {
     "critical": logging.CRITICAL,
@@ -44,7 +44,7 @@ LOG_LEVELS = {
 
 def set_logging(level, handler=None):
     """
-    Set level of logging, and choose where to display/save logs 
+    Set level of logging, and choose where to display/save logs
     (file or standard output).
     """
     if not handler:
@@ -55,7 +55,7 @@ def set_logging(level, handler=None):
     logger.setLevel(loglevel)
     format = r"%(asctime)s [%(process)d] [%(levelname)s] %(message)s"
     datefmt = r"%Y-%m-%d %H:%M:%S"
-    
+
     handler.setFormatter(logging.Formatter(format, datefmt))
     logger.addHandler(handler)
 
--- couchdbkit/version.py
+++ couchdbkit/version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -
 #
-# This file is part of couchdbkit released under the MIT license. 
+# This file is part of couchdbkit released under the MIT license.
 # See the NOTICE for more information.
 
-version_info = (0, 5, 7)
+version_info = (0, 6, 0)
 __version__ =  ".".join(map(str, version_info))
