97739b60bf614a91960a1962a248cbb8aa1fc9be
Daniel Roberts <droberts@dimagi.com>
2013-08-28 14:12:01 -0400

    clean up properties.py

--- couchdbkit/schema/properties.py
+++ couchdbkit/schema/properties.py
@@ -5,12 +5,14 @@
 import functools
 from jsonobject.properties import *
 from jsonobject.base import DefaultProperty
-from jsonobject.convert import ALLOWED_PROPERTY_TYPES
+from jsonobject.convert import (
+    ALLOWED_PROPERTY_TYPES,
+    MAP_TYPES_PROPERTIES,
+    value_to_python,
+    value_to_property
+)
 
-SchemaProperty = ObjectProperty
-SchemaListProperty = ListProperty
 StringListProperty = functools.partial(ListProperty, unicode)
-SchemaDictProperty = DictProperty
 
 
 class Property(DefaultProperty):
@@ -33,14 +35,15 @@ class Property(DefaultProperty):
         raise NotImplementedError()
 
 
-dict_to_json = None
-list_to_json = None
-value_to_json = None
-value_to_python = None
-dict_to_python = None
-list_to_python = None
-convert_property = None
-value_to_property = None
+def _not_implemented(*args, **kwargs):
+    raise NotImplementedError()
+
+dict_to_json = _not_implemented
+list_to_json = _not_implemented
+value_to_json = _not_implemented
+dict_to_python = _not_implemented
+list_to_python = _not_implemented
+convert_property = _not_implemented
 
 LazyDict = JsonDict
 LazyList = JsonArray
